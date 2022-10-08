1bbd2c10c98762027a2ce43fc89cd56ecea94fde
Md. Rubayet Hossain <rubayet33@gmail.com>
2022-10-08 23:55:38 +0600

    feat: install typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App = (): JSX.Element => {
   return (
     <div className="text-2xl font-bold text-center mt-24 uppercase p-1 bg-slate-400">
       Journey to Storybook
     </div>
   );
-}
+};
 
 export default App;
